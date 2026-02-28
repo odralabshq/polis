1c031df8eb6b37f1f36eb5004943060c1d56d7ab
Tomasz Krakowiak <tkrakowiak@gmail.com>
2026-02-28 10:43:45 +0100

    feat: removing old tests

--- cli/tests/unit/main.rs
+++ cli/tests/unit/main.rs
@@ -1,20 +1,3 @@
 //! Unit tests for polis CLI
-//!
-//! These tests use mocked dependencies and run fast without external I/O.
 
-pub mod mocks;
-
-mod agent_command;
-mod agent_crud_service;
 mod architecture;
-mod config_command;
-mod doctor_command;
-mod doctor_service;
-mod helpers;
-mod output;
-mod property_tests;
-mod provisioner_tests;
-mod start_command;
-mod start_stop_delete;
-mod status_command;
-mod workspace_start_service;
